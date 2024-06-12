ded2ebdda74fef06cd8051721219062473edc604
kacperden7 <kacperden7@gmail.com>
2024-06-12 18:51:51 +0200

    update: chat in bot

--- src/bot/handlers/admin_handlers.py
+++ src/bot/handlers/admin_handlers.py
@@ -1,11 +1,14 @@
+import logging
 from aiogram import types
 from aiogram.dispatcher import FSMContext
-from src.bot.bot_setup import bot, dp
+from aiogram.types import ContentTypes
+from src.bot.bot_setup import bot, dp, database
 from src.db.models.db_models import User
 from src.bot.config import ADMIN_CHAT_ID
 from src.utils.keyboards import admin_main_menu
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from src.utils.payment_utils import get_payment_id_from_callback
+from src.db.repositories.user_repositories import UserRepository
+from src.middlewares.forward_to_admin_middleware import forwarded_message_mapping
 
 class AdminStates(StatesGroup):
     waiting_for_message = State()
@@ -14,9 +17,17 @@ class AdminStates(StatesGroup):
 async def admin_start_command(message: types.Message):
     await message.reply("Welcome, admin!", reply_markup=admin_main_menu())
 
+# @dp.message_handler(commands=['start'])
+# async def admin_start_command(message: types.Message):
+#     if message.from_user.id == ADMIN_CHAT_ID:
+#         await message.reply("Welcome to ADMIN!", reply_markup=admin_main_menu())
+
 @dp.message_handler(lambda message: message.from_user.id == ADMIN_CHAT_ID and message.text == "👥 My Clients")
 async def admin_my_clients_command(message: types.Message):
-    clients = User.select()
+    with database.get_session() as session:
+        user_repository = UserRepository(session) 
+        clients = user_repository.get_all_users()  # Use the repository method
+
     if not clients:
         await message.reply("No clients found.")
     else:
@@ -31,15 +42,68 @@ async def admin_my_clients_command(message: types.Message):
 @dp.callback_query_handler(lambda query: query.from_user.id == ADMIN_CHAT_ID and query.data.startswith("client_"))
 async def admin_client_selected(query: types.CallbackQuery, state: FSMContext):
     client_id = int(query.data.split("_")[1])
-    client = User.get_by_id(client_id)
-    await state.update_data(selected_client=client)
-    await AdminStates.waiting_for_message.set()
-    await query.message.reply("Please enter the message you want to send to the client:")
+
+    with database.get_session() as session:
+        user_repository = UserRepository(session)
+        client = user_repository.get_user_by_id(client_id)  # Use repository method
+
+    if client:
+        await state.update_data(selected_client=client)
+        await AdminStates.waiting_for_message.set()
+        await query.message.reply("Please enter the message you want to send to the client:")
+    else:
+        await query.message.reply("Client not found.")
 
 @dp.message_handler(lambda message: message.from_user.id == ADMIN_CHAT_ID, state=AdminStates.waiting_for_message)
 async def admin_send_message_to_client(message: types.Message, state: FSMContext):
     data = await state.get_data()
     client = data.get('selected_client')
-    await bot.send_message(chat_id=client.telegram_chat_id, text=message.text)
-    await message.reply(f"Message sent to {client.first_name} {client.last_name} (@{client.username}).")
-    await state.finish()
+
+    if client:
+        await bot.send_message(chat_id=client.telegram_chat_id, text=message.text)
+        await message.reply(
+            f"Message sent to {client.first_name} {client.last_name} "
+            f"(@{client.username}, Telegram ID: {client.telegram_user_id})."  # Added Telegram ID
+        )
+        await state.finish()
+    else:
+        await message.reply("Client not found or not selected.")
+
+# @dp.message_handler(lambda message: message.reply_to_message and message.from_user.id == ADMIN_CHAT_ID)
+@dp.message_handler(content_types=ContentTypes.ANY)
+async def handle_admin_reply(message: types.Message):
+    if message.reply_to_message and message.from_user.id == ADMIN_CHAT_ID:
+        original_message = forwarded_message_mapping.get(message.reply_to_message.message_id)
+        if original_message:
+            original_user_chat_id = original_message.chat.id
+            try:
+                if message.content_type == 'text':
+                    await bot.send_message(chat_id=original_user_chat_id, text=message.text)
+                elif message.content_type == 'photo':
+                    photo = message.photo[-1]
+                    await bot.send_photo(chat_id=original_user_chat_id, photo=photo.file_id, caption=message.caption)
+                elif message.content_type == 'document':
+                    await bot.send_document(chat_id=original_user_chat_id, document=message.document.file_id, caption=message.caption)
+                elif message.content_type == 'sticker':
+                    await bot.send_sticker(chat_id=original_user_chat_id, sticker=message.sticker.file_id)
+                elif message.content_type == 'audio':
+                    await bot.send_audio(chat_id=original_user_chat_id, audio=message.audio.file_id, caption=message.caption)
+                elif message.content_type == 'video':
+                    await bot.send_video(chat_id=original_user_chat_id, video=message.video.file_id, caption=message.caption)
+                elif message.content_type == 'voice':
+                    await bot.send_voice(chat_id=original_user_chat_id, voice=message.voice.file_id, caption=message.caption)
+                elif message.content_type == 'contact':
+                    await bot.send_contact(chat_id=original_user_chat_id, phone_number=message.contact.phone_number, first_name=message.contact.first_name, last_name=message.contact.last_name)
+                elif message.content_type == 'location':
+                    await bot.send_location(chat_id=original_user_chat_id, latitude=message.location.latitude, longitude=message.location.longitude)
+                elif message.content_type == 'venue':
+                    await bot.send_venue(chat_id=original_user_chat_id, latitude=message.venue.location.latitude, longitude=message.venue.location.longitude, title=message.venue.title, address=message.venue.address)
+                else:
+                    await bot.send_message(chat_id=original_user_chat_id, text="Unsupported content type.")
+                
+                await message.reply("Your reply has been sent to the user.")
+            except Exception as e:
+                logging.exception(f"Error sending reply to user: {e}")
+                await message.reply("Failed to send the reply to the user.")
+        else:
+            await message.reply("The original user message could not be found.")
--- src/bot/handlers/client_handlers.py
+++ src/bot/handlers/client_handlers.py
@@ -2,10 +2,10 @@ from aiogram import types
 from functools import partial
 from aiogram.dispatcher.filters import Command, Text
 
-from src.bot.bot_setup import dp
-from src.utils.keyboards import client_main_menu
+#from src.bot.bot_setup import dp, database
+from src.db.repositories.proxy_repositories import ProxyRepository
+from src.utils.keyboards import client_main_menu, generate_connection_selection_keyboard
 from src.utils.proxy_utils import get_user_proxies
-from src.db.database import Database
 from src.db.repositories.user_repositories import UserRepository
 from src.bot.config import SECRET_NAME, REGION_NAME, DATABASE_NAME, DATABASE_HOST, ADMIN_CHAT_ID
 from src.utils.helpers import forward_message_to_admin
@@ -13,38 +13,30 @@ from src.bot.handlers.payment_handlers import *
 from src.utils.payment_utils import *
 from src.services.payment_service import *
 
-@dp.message_handler(commands=['start'])
+
+@dp.message_handler(lambda message: message.from_user.id != ADMIN_CHAT_ID, commands=['start'])
 async def admin_start_command(message: types.Message):
-    if message.from_user.id != ADMIN_CHAT_ID:
-        await message.reply("Welcome to ProxyHelper!", reply_markup=client_main_menu())
+    await message.reply("Welcome to ProxyBroker Helper!", reply_markup=client_main_menu())
 
-# client_handlers.py
 @dp.message_handler(lambda message: message.text == "💳 Pay")
-async def handle_pay_command(message: types.Message):
-    database = dp.get('database')
-    user_repository = UserRepository(database)
-    user = await user_repository.get_or_create_user(message)
+async def handle_pay_command(message: types.Message, state: FSMContext):
+    with database.get_session() as session:
+        user_repository = UserRepository(session)
+        connection_repository = ConnectionRepository(session)
 
-    if user:
-        await user_repository.update_user(user, message)
+        user = user_repository.get_or_create_user(message)
+        if user is None:
+            await message.answer("Failed to retrieve user information.")
+            return
 
-        proxies = get_user_proxies(user.username, user.telegram_chat_id, user.telegram_user_id)  # Fetch proxies associated with the user
-        if proxies:
-            keyboard = types.InlineKeyboardMarkup()
-            selected_proxy_ids = []  # Keep track of selected proxy IDs
-            for proxy in proxies:
-                button_text = f"{proxy.name}"
-                keyboard.add(types.InlineKeyboardButton(text=button_text, callback_data=f"select_proxy:{proxy.id}"))
-            keyboard.add(types.InlineKeyboardButton(text="Pay", callback_data="pay_selected_proxies"))
-            message_text = "Select the proxies you want to pay for:"
-            await message.answer(message_text, reply_markup=keyboard)
+        user_repository.update_user(user, message)
+
+        connections = connection_repository.get_user_connections(user.id)
+        if connections:
+            await state.update_data(selected_connection_ids=[])  # Initialize selected connections
+            await state.update_data(user_id=user.id)  # Store user_id in state data
+            await state.update_data(telegram_user_id=message.from_user.id) 
+            keyboard = generate_connection_selection_keyboard(connections, user_id=user.id)  # Pass user_id to the keyboard generator
+            await message.answer("Select the connections you want to pay for:", reply_markup=keyboard)
         else:
-            await message.answer("You currently have no proxies to pay for.")
-    else:
-        await message.answer("Failed to retrieve user information.")
-
-
-@dp.message_handler(content_types=types.ContentTypes.ANY)
-async def handle_client_message(message: types.Message):
-    if message.chat.id != ADMIN_CHAT_ID:
-        await forward_message_to_admin(message)
+            await message.answer("You currently have no connections to pay for.")
