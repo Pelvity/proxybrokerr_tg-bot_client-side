a662a352eb8e5c8b75b031282c13a6db52e7b358
Mediasm <media.smolii001@gmail.com>
2024-07-25 01:25:46 +0200

    update: use_ssh

--- src/bot/config.py
+++ src/bot/config.py
@@ -52,7 +52,7 @@ SSH_HOST = os.getenv('SSH_HOST')
 SSH_PORT = int(os.getenv('SSH_PORT', 22))
 SSH_USER = os.getenv('SSH_USER')
 SSH_PKEY = os.getenv('SSH_PKEY')
-USE_SSH = os.environ.get('USE_SSH').lower() == 'true'
+USE_SSH = os.environ.get('USE_SSH')
 
 if DATABASE_TYPE == "azure":
     SQL_CONNECTIONSTRING = URL.create(
